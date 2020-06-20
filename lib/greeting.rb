35593231835c9c99456a58f513fc51e185507c1f
Erik Zhao <erikzhao98@gmail.com>
2020-06-20 16:43:41 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,14 +1,5 @@
 def greeting
   puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
 end
 
 greeting
